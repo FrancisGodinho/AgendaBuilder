a3df34d74a30df69eb7fba5a83c304cf6f97f435
francisgodinho <francisgodinho2010@hotmail.com>
2020-05-04 17:30:16 -0700

    started testing timetable

--- src/test/java/timetable_test/TimeTableTest.java
+++ src/test/java/timetable_test/TimeTableTest.java
@@ -13,6 +13,42 @@ public class TimeTableTest {
 
     @Test
     public void addRemoveTest(){
+        TimeInstance time1 = new TimeInstance(Day.MON, 9, 0);
+        TimeInstance time2 = new TimeInstance(Day.MON, 12, 0);
+
+        TimeInstance time3 = new TimeInstance(Day.MON, 12, 0);
+        TimeInstance time4 = new TimeInstance(Day.MON, 14, 0);
+
+        TimeInstance time5 = new TimeInstance(Day.MON, 11, 0);
+        TimeInstance time6 = new TimeInstance(Day.MON, 13, 0);
+
+        TimeInstance time7 = new TimeInstance(Day.MON, 12, 0);
+        TimeInstance time8 = new TimeInstance(Day.MON, 13, 0);
+
+        UBC_CourseSection sec1 = new UBC_CourseSection(201, "", "L2A", "");
+        UBC_CourseSection sec2 = new UBC_CourseSection(202, "", "L2B", "");
+        UBC_CourseSection sec3 = new UBC_CourseSection(201, "", "", "D2B");
+        UBC_CourseSection sec4 = new UBC_CourseSection(202, "", "", "D2A");
+
+        UBC_CourseActivity course1 = new UBC_CourseActivity("Elec", 201, sec1, time1, time2);
+        UBC_CourseActivity course2 = new UBC_CourseActivity("Elec", 201, sec2, time3, time4);
+        UBC_CourseActivity course3 = new UBC_CourseActivity("Math", 253, sec3, time5, time6);
+        UBC_CourseActivity course4 = new UBC_CourseActivity("Math", 253, sec4, time7, time8);
+
+
+        TimeTable timeTable = new TimeTable();
+
+        timeTable.addActivity(course1);
+        Assert.assertTrue(timeTable.doesConflict(course1));
+        Assert.assertFalse(timeTable.doesConflict(course2));
+        Assert.assertTrue(timeTable.doesConflict(course3));
+        Assert.assertFalse(timeTable.doesConflict(course4));
+
+        timeTable.removeActivity(new UBC_CourseActivity("Elec", 201, sec1, time1, time2));
+        Assert.assertFalse(timeTable.doesConflict(course1));
+        Assert.assertFalse(timeTable.doesConflict(course2));
+        Assert.assertFalse(timeTable.doesConflict(course3));
+        Assert.assertFalse(timeTable.doesConflict(course4));
 
     }
 
